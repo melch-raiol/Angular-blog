92b20326a930dbb86bf37bb48a268bffe02e1cd3
melch <melchraiol@hotmail.com>
2023-09-18 10:41:45 -0300

    content page finished

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {dataBase} from '../../data/dataBase'
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  smallCardCover1:string = ""
+  @Input()
+  smallCardTitle1:string = ""
+  @Input()
+  smallCardDescription1:string = ""
+
+  @Input()
+  smallCardCover2:string = ""
+  @Input()
+  smallCardTitle2:string = ""
+  @Input()
+  smallCardDescription2:string = ""
+
+  @Input()
+  smallCardCover3:string = ""
+  @Input()
+  smallCardTitle3:string = ""
+  @Input()
+  smallCardDescription3:string = ""
+
+  private Id:string | null = "0"
+
+  constructor(
+    private route: ActivatedRoute
+  ) {  }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(value =>
+      this.Id = value.get("id")
+      )
+      this.setValuesToComponent(this.Id)
+  }
+
+  setValuesToComponent(id:string | null){
+
+    this.smallCardTitle1 = dataBase[1].title
+    this.smallCardDescription1 = dataBase[1].description
+    this.smallCardCover1 = dataBase[1].photoCover
+
+    this.smallCardTitle2 = dataBase[2].title
+    this.smallCardDescription2 = dataBase[2].description
+    this.smallCardCover2 = dataBase[2].photoCover
+
+    this.smallCardTitle3 = dataBase[3].title
+    this.smallCardDescription3 = dataBase[3].description
+    this.smallCardCover3 = dataBase[3].photoCover
   }
 
 }
